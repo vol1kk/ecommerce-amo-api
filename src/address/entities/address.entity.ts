1c1dc0b0e2576953bbaadbb44d01bc4eaf336e14
Vsevolod <thebrokennworld@gmail.com>
2024-01-08 14:36:23 +0200

    feat: created /address route

---
+++ src/address/entities/address.entity.ts
@@ -0,0 +1,12 @@
+export class Address {
+  id: string;
+  name: string;
+  surname: string;
+  city: string;
+  address: string;
+  postalCode: string;
+  phone: string;
+  isDefault: boolean;
+  tags: string[];
+  userId?: string;
+}
