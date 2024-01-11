e9f5974e4f498767e87de84d18f31f01da5d7e67
Vsevolod <thebrokennworld@gmail.com>
2024-01-11 10:11:54 +0200

    feat: addressed posted to provided by token id; fix: omitted userId from dto

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,21 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { REQUEST } from "@nestjs/core";
+import { Inject, Injectable, Scope } from "@nestjs/common";
 
 import { DatabaseService } from "@/database/database.service";
 import { CreateAddressDto } from "@/address/dto/create-address.dto";
 import { UpdateAddressDto } from "@/address/dto/update-address.dto";
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class AddressService {
-  constructor(private db: DatabaseService) {}
+  constructor(
+    private db: DatabaseService,
+    @Inject(REQUEST) private readonly request: Request,
+  ) {}
 
   create(createAddressDto: CreateAddressDto) {
+    const createdBy = this.request["user"]?.id as string | undefined;
+
     return this.db.address.create({
-      data: createAddressDto,
+      data: {
+        ...createAddressDto,
+        userId: createdBy,
+      },
     });
   }
 
   findAll() {
-    return this.db.address.findMany();
+    const createdBy = this.request["user"]?.id as string | undefined;
+
+    return this.db.address.findMany({ where: { userId: createdBy } });
   }
 
   findOne(id: string) {
@@ -23,15 +34,20 @@ export class AddressService {
   }
 
   async update(id: string, updateAddressDto: UpdateAddressDto) {
+    const createdBy = this.request["user"]?.id as string | undefined;
+
     return this.db.address.update({
       where: {
         id,
+        userId: createdBy,
       },
       data: updateAddressDto,
     });
   }
 
   async remove(id: string) {
-    return this.db.address.delete({ where: { id } });
+    const createdBy = this.request["user"]?.id as string | undefined;
+
+    return this.db.address.delete({ where: { id, userId: createdBy } });
   }
 }
